e2726b33cca432dea0116b8a8203b1a80d37a4db
hasansaad1 <hasansaad.saad1998@gmail.com>
2017-12-13 17:58:00 +0200

    adding the renderingposts function reference #20

--- public/index.js
+++ public/index.js
@@ -1,12 +1,30 @@
-document.getElementById('login').addEventListener('click',function(){
-  alert('clicked');
-})
+console.log('i run');
 
-// document.getElementById('login_form').addEventListener('submit' , function(){
-// var pd = document.getElementById('password').value;
-// var un = document.getElementById('user_name').value;
-//
-// alert(pd , un);
-//
-//
-// })
+function displayPosts(arr) {
+  console.log('arr:', arr);
+  var container = document.getElementById('container');
+  arr.forEach(function(post) {
+    var div = document.createElement('div');
+    var title = document.createElement('h2');
+    title.textContent = post.title;
+    var content = document.createElement('p');
+    content.textContent = post.content;
+    var author = document.createElement('span');
+    div.appendChild(title);
+    div.appendChild(content);
+    container.appendChild(div);
+  });
+}
+
+(function() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log(xhr.responseText);
+      displayPosts(JSON.parse(xhr.responseText));
+    }
+  };
+
+  xhr.open("GET", '/posts');
+  xhr.send();
+})()
